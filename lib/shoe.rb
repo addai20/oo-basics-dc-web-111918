33404f84f8494ae79d1a221759a4a911160ea241
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-25 19:13:08 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,5 +6,24 @@ class Shoe
     @brand = brand
   end
   
+  def color=(color)
+    @color = color
+  end
+  
+  def size=(size)
+    @size = size
+  end
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def cobble
+    
+  end
 
 end
